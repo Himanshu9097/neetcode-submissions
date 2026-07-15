2818c1f99cd32212025fef09f8fa35856b1d06d6
Himanshu kumar <74715122+Himanshu9097@users.noreply.github.com>
2026-07-15 16:29:53 +0530

    Add: count-paths - submission-0

---
+++ Data Structures & Algorithms/count-paths/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int slove(int i , int j , vector<vector<int>>&mat,vector<vector<int>>&dp){
+        int m = mat.size();
+        int n = mat[0].size();
+
+        if(i >= m || j >= n) return 0;
+        if(i == m-1 && j == n - 1) return 1;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        // move down
+        int down = slove(i+1,j,mat,dp);
+
+        // move right
+        int right = slove(i,j+1,mat,dp);
+
+        return dp[i][j] = down + right;
+    }
+
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> mat(m,vector<int>(n,'.'));
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+
+        return slove(0,0,mat,dp);
+    }
+};
